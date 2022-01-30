b154b5c4c190d30e7e9488ba95b9e717709af5b3
Phil Garner <phil.b.garner@gmail.com>
2022-01-29 21:36:04 -0500

    Initial commit.

---
+++ index.js
@@ -0,0 +1,192 @@
+const session = require('express-session');
+const express = require('express');
+const http = require('http');
+const uuid = require('uuid');
+const bodyParser = require('body-parser')
+
+const WebSocket = require('ws');
+
+const app = express();
+const users = require('./lib/users')
+
+//
+// We need the same instance of the session parser in express and
+// WebSocket server.
+//
+const sessionParser = session({
+  saveUninitialized: false,
+  secret: '$eCuRiTy',
+  resave: false
+});
+
+//
+// Serve static files from the 'public' folder.
+//
+app.use(express.static('public'));
+app.use(sessionParser);
+app.use(bodyParser.json())
+
+app.post('/login', function (req, res) {
+  //
+  // "Log in" user and set userId to session.
+  //
+  let user = require('./lib/users').getUser(req.body.username)
+  if (user !== null) {
+    if (user.password === req.body.password) {
+      req.session.userId = user.uuid
+      res.send({ ok: true, message: `Login successful.`})
+      return
+    }
+  }
+  res.send({ ok: false, message: `Login failed.`})
+});
+
+app.post('/register', function (req, res) {
+  if (users.getUser(req.body.username) !== null) {
+    res.send({ ok: false, message: `User ${req.body.username} already exists. Please use another username.`})
+    return
+  }
+
+  const id = uuid.v4();
+  let User = require('./lib/user').user
+  let user = new User(id, req.body.username, req.body.password)
+  users.addUser(user)
+  res.send({ok: true, message: `User ${user.username} created.`})
+  let data = require('./lib/data')
+  data.save(false, false, true)
+})
+
+app.delete('/logout', function (request, response) {
+  const ws = users.getUserWs(request.session.userId);
+  console.log('Destroying session');
+  request.session.destroy(function () {
+    if (ws) ws.close();
+
+    response.send({ result: 'OK', message: 'Session destroyed' });
+  });
+});
+
+//
+// Create an HTTP server.
+//
+const server = http.createServer(app);
+
+// Initiate MUD Entity lists and handlers
+const welcome = require('./lib/welcome')
+
+const Handler = require('./lib/handler')
+const Player = require('./lib/player')
+
+const addPlayer = require('./lib/entities').addPlayer
+const removePlayer = require('./lib/entities').removePlayer
+
+//
+// Handle Event Registrations
+//
+var handler = new Handler()
+
+const Look = require('./lib/commands/look')
+const CreateRoom = require('./lib/commands/createroom')
+const ListRooms = require('./lib/commands/listrooms')
+const LinkExit = require('./lib/commands/linkexit');
+const Tp = require('./lib/commands/tp');
+const DescribeRoom = require('./lib/commands/describeroom');
+const DescribeMe = require('./lib/commands/describeme');
+const Me = require('./lib/commands/me');
+const Save = require('./lib/commands/save');
+const Reload = require('./lib/commands/load');
+const ListPlayers = require('./lib/commands/listplayers');
+const Impersonate = require('./lib/commands/impersonate');
+
+handler.AddHandler(new Look('Look'))
+handler.AddHandler(new CreateRoom('Create Room'))
+handler.AddHandler(new ListRooms('List Room'))
+handler.AddHandler(new LinkExit('Link Exit'))
+handler.AddHandler(new Tp('Tp'))
+handler.AddHandler(new DescribeRoom('Describe Room'))
+handler.AddHandler(new DescribeMe('Describe Me'))
+handler.AddHandler(new Me('Me'))
+handler.AddHandler(new Save('Save'))
+handler.AddHandler(new Reload('Reload'))
+handler.AddHandler(new ListPlayers('List Players'))
+handler.AddHandler(new Impersonate('Impersonate'))
+handler.AddHandler(new LinkExit('Link Exit'))
+
+// Load data files if there are any.
+function loadData () {
+  console.log('Loading data files...')
+  return new Promise((resolve, reject) => {
+    require('./lib/data').load().then(() => {
+      console.log('Data load complete!')
+      resolve()
+    }).catch(() => {
+      console.log('Data load failed.')
+      reject()
+    })
+  })
+}
+
+//
+// Create a WebSocket server completely detached from the HTTP server.
+//
+const wss = new WebSocket.Server({ clientTracking: false, noServer: true });
+
+server.on('upgrade', function (request, socket, head) {
+  sessionParser(request, {}, () => {
+    if (!request.session.userId) {
+      socket.write('HTTP/1.1 401 Unauthorized\r\n\r\n');
+      socket.destroy();
+      return;
+    }
+
+    wss.handleUpgrade(request, socket, head, function (ws) {
+      wss.emit('connection', ws, request);
+    });
+  });
+});
+
+wss.on('connection', function (ws, request) {
+  const userId = request.session.userId;
+  users.setUserWs(userId, ws)
+  const players = require('./lib/entities')
+  let newPlayer = false
+  let player = players.getPlayer(userId)
+  if (player === null) {
+    player = new Player({ uuid: userId, name: 'Spirit', description: 'A formless spirit.' })
+    newPlayer = true
+    addPlayer(player)
+  } 
+    
+  // Send the welcome message when the user connects.
+  welcome.send(ws)
+
+  if (!newPlayer) {
+    ws.send(`Welcome back ${player.name}!`)
+  }
+
+  ws.on('message', function (message) {
+    //
+    // Handle input from the user client.
+    //
+    if (!handler.Parse(ws, userId, message.toString())) {
+      if (!handler.DefaultHandler(ws, userId, message.toString())) {
+        ws.send(`Invalid command '${command}'`)
+      }
+    }
+  });
+
+  ws.on('close', function () {
+    users.deleteUserWs(userId)
+    removePlayer(userId)
+  });
+});
+
+//
+// Load data and then start the server.
+//
+loadData().then(() =>
+  server.listen(8080, function () {
+    console.log('Listening on http://localhost:8080');
+    let saveData = require('./lib/data').save
+    let autoSave = setInterval(() => saveData(true, true, true).then(() => console.log('Autosave complete.')), require('./lib/config.json').autosaveTimeout)
+  }))
---
+++ public/app.js
@@ -0,0 +1,137 @@
+(function () {
+    const messages = document.querySelector('#messages')
+    const wsButton = document.querySelector('#wsButton')
+    const wsSendButton = document.querySelector('#wsSendButton')
+    //const logout = document.querySelector('#logout')
+    const login = document.querySelector('#login')
+    const command = document.querySelector('#command')
+    const register = document.getElementById('btnRegister')
+    const showregister = document.getElementById('showregister')
+
+    const loginPanel = document.querySelector('#loginpanel')
+    const registerPanel = document.getElementById('registerpanel')
+
+    const username = document.querySelector('#username')
+    const password = document.getElementById('passw')
+    const regusername = document.querySelector('#regusername')
+    const regpassword = document.getElementById('regpassw')
+
+    toggleLoginPanel()
+
+    function toggleLoginPanel() {
+      if (loginPanel.classList.contains('showlogin')) {
+        loginPanel.classList.remove('showlogin')
+      } else {
+        loginPanel.classList.add('showlogin')
+      }
+    }
+
+    function toggleRegisterPanel() {
+      if (registerPanel.classList.contains('showregister')) {
+        registerPanel.classList.remove('showregister')
+      } else {
+        registerPanel.classList.add('showregister')
+      }
+    }
+  
+    function showMessage(response) {
+      messages.textContent += `\n${response.message}`
+      messages.scrollTop = messages.scrollHeight
+    }
+
+    showregister.onclick = function () {
+      toggleLoginPanel()
+      toggleRegisterPanel()
+    }
+
+    register.onclick = function () {
+      fetch('/register', { method: 'POST', credentials: 'same-origin', headers: { 'Content-Type': 'application/json' },
+                          body: JSON.stringify({ username: regusername.value, password: regpassword.value })})
+      .then(function (res) {
+        if (res.ok) {
+          toggleRegisterPanel()
+          toggleLoginPanel()
+          return res.json()
+        }
+      }).then(showMessage)
+      .catch(function (err) {
+        showMessage(err.message)
+      })
+    }
+
+    login.onclick = function () {
+        fetch('/login', { method: 'POST', credentials: 'same-origin', headers: { 'Content-Type': 'application/json' },
+                          body: JSON.stringify({ username: username.value, password: password.value }) })
+        .then(function (res) {
+          if (res.ok) {
+            toggleLoginPanel()
+            return res.json()
+          }
+          else {
+            return {ok: false, message: 'Login failed: username or password is incorrect.'}
+          }
+        })
+        .then(showMessage)
+        .then(webSocketConnect)
+        .catch(function (err) {
+          showMessage(err.message)
+        })
+    }
+  
+    // logout.onclick = function () {
+    //   fetch('/logout', { method: 'DELETE', credentials: 'same-origin' })
+    //     .then(handleResponse)
+    //     .then(showMessage)
+    //     .catch(function (err) {
+    //       showMessage(err.message)
+    //     })
+    // }
+  
+    let ws
+  
+    function webSocketConnect () {
+      if (ws) {
+        ws.onerror = ws.onopen = ws.onclose = null
+        ws.close()
+      }
+  
+      ws = new WebSocket(`ws://${location.host}`)
+      ws.onerror = function () {
+        showMessage({ok: false, message: 'WebSocket error'})
+      }
+      ws.onopen = function () {
+        showMessage({ok: true, message: 'WebSocket connection established'})
+      }
+      ws.onclose = function () {
+        showMessage({ok: true, message: 'WebSocket connection closed'})
+        ws = null
+      }
+      ws.onmessage = function (e) {
+        showMessage({ok: true, message: e.data})
+      }
+    }
+  
+    wsSendButton.onclick = function () {
+      if (!ws) {
+        showMessage({ok: false, message: 'No WebSocket connection'})
+        return
+      }
+  
+      ws.send(command.value)
+      command.value = ''
+    }
+
+    command.addEventListener('keypress', (e) => {
+      if (e.code === 'Enter') {
+        if (!ws) {
+          showMessage({ok: false, message: 'No WebSocket connection'})
+          return
+        }
+
+        ws.send(command.value)
+        command.value = ''
+      }
+    })
+
+  })()
+  
